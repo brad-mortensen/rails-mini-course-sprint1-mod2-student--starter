082c6e2e8587277de5144685ab1956d48765f085
Brad Mortensen <b.mort86@gmail.com>
2019-08-14 12:22:00 -0600

    adds recommended class method

--- book.rb
+++ book.rb
@@ -9,3 +9,11 @@ class Book
     @count = 3
   end
 end
+
+def Book.recommended_books
+  [
+    Book.new("The Well-Grounded Rubyist", "David A. Black"),
+    Book.new("Practical Object-Oriented Design in Ruby", "Sandi Metz"),
+    Book.new("Effective Testing with RSpec 3", "Myron Marston"),
+  ]
+end
